c2882ca8c9930efa149f944eca50cf9f1bbe68bb
Ajzboss <48899953+Ajzboss@users.noreply.github.com>
2025-06-11 19:51:03 -0700

    Remove api

--- airline-checkin-app/server/index.js
+++ airline-checkin-app/server/index.js
@@ -135,10 +135,10 @@ const redisRouter = require("./routes/redis");
 // app.use("/checkin",  checkinRouter);
 // app.use("/redis", redisRouter);
 
-app.use("api/flights", flightsRouter); 
-app.use("api/users",   usersRouter);
-app.use("api/checkin",  checkinRouter);
-app.use("api/redis", redisRouter);
+app.use("/flights", flightsRouter); 
+app.use("/users",   usersRouter);
+app.use("/checkin",  checkinRouter);
+app.use("/redis", redisRouter);
 
 app.get("/test", async (req, res) => {
   const snapshot = await db.collection("flights").get();
